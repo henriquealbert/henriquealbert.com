c423875b8c783c7fb21f11ef129a9e7d171f6daa
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-01-31 16:13:13 -0300

    :art: add prettier and eslint config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['var(--font-kaisei)']
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: [require('@tailwindcss/typography')]
+}
