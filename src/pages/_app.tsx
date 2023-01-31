4e2694f4a5febfb07c420cf9c4662ae07164ed89
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-01-31 17:47:19 -0300

    :lipstick: configure font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { type AppType } from 'next/dist/shared/lib/utils'
+import { Roboto_Slab } from '@next/font/google'
 
 import '../styles/globals.css'
 
+const robotoSlab = Roboto_Slab({
+  subsets: ['latin'],
+  variable: '--font-roboto-slab'
+})
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${robotoSlab.variable} font-serif`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
-        serif: ['var(--font-kaisei)']
+        serif: ['var(--font-roboto-slab)', 'serif'],
+        sans: ['Inter', 'sans-serif']
       }
     }
-  },
-  plugins: [require('@tailwindcss/typography')]
+  }
 }
